073f955e8c0ab221849aa4f97ee0189850b8490f
aegorsuch <alex@weartak.com>
2026-03-18 17:12:04 -0500

    Add HoundmasterDropDownReceiver to show dashboard on plugin icon click; refactor overlay logic

---
+++ app/src/main/java/com/atakmap/android/helloworld/plugin/HoundmasterDropDownReceiver.java
@@ -0,0 +1,25 @@
+package com.atakmap.android.helloworld.plugin;
+
+import android.content.Context;
+import android.content.Intent;
+import com.atakmap.android.dropdown.DropDownReceiver;
+import com.atakmap.android.maps.MapView;
+
+public class HoundmasterDropDownReceiver extends DropDownReceiver {
+    private final Context pluginContext;
+    private final MapView mapView;
+
+    public HoundmasterDropDownReceiver(MapView mapView, Context pluginContext) {
+        super(mapView);
+        this.pluginContext = pluginContext;
+        this.mapView = mapView;
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        // Show the Houndmaster Dashboard when the plugin icon is clicked
+        BloodhoundDashboardDropDown dash = new BloodhoundDashboardDropDown(mapView, pluginContext, BloodhoundOrderManager.getInstance());
+        dash.show();
+    }
+}
+
